e726142f6a46e904fd24c9c236166861635d70cc
Henry Yan <yanhonglei@gmail.com>
2013-01-01 12:28:58 +0800

    测试一些bug

---
+++ src/test/java/org/activiti/designer/test/ProcessTestProcess13.java
@@ -0,0 +1,43 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestProcess13 {
+
+	private String filename = "/Users/henryyan/work/projects/activiti/activiti-study/src/main/resources/diagrams/qun/process_16.bpmn";
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+
+	@Test
+	public void startProcess() throws Exception {
+		RepositoryService repositoryService = activitiRule.getRepositoryService();
+		repositoryService.createDeployment().addInputStream("process_16.bpmn20.xml",
+				new FileInputStream(filename)).deploy();
+		
+		ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().singleResult();
+		InputStream resourceAsStream = repositoryService.getResourceAsStream(processDefinition.getDeploymentId(), processDefinition.getDiagramResourceName());
+		assertNotNull(resourceAsStream);
+		
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("name", "Activiti");
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process_16", variableMap);
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+}
