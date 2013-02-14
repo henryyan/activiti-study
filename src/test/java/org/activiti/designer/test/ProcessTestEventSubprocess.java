95c78c95aa9c51d179bb33b11b5a85076184346f
Henry Yan <yanhonglei@gmail.com>
2013-02-14 22:45:30 +0800

    添加事件子流程例子

---
+++ src/test/java/org/activiti/designer/test/ProcessTestEventSubprocess.java
@@ -0,0 +1,46 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestEventSubprocess {
+
+  private String filename = "/Users/henryyan/work/projects/activiti/activiti-study/src/test/resources/diagrams/EventSubprocess.bpmn";
+
+  @Rule
+  public ActivitiRule activitiRule = new ActivitiRule();
+
+  @Test
+  public void startProcess() throws Exception {
+    RepositoryService repositoryService = activitiRule.getRepositoryService();
+    repositoryService.createDeployment().addInputStream("EventSubprocess.bpmn20.xml", new FileInputStream(filename)).deploy();
+    RuntimeService runtimeService = activitiRule.getRuntimeService();
+    Map<String, Object> variableMap = new HashMap<String, Object>();
+    variableMap.put("name", "Activiti");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("EventSubprocess", variableMap);
+    assertNotNull(processInstance.getId());
+    System.out.println("id " + processInstance.getId() + " " + processInstance.getProcessDefinitionId());
+
+    TaskService taskService = activitiRule.getTaskService();
+    Task task = taskService.createTaskQuery().singleResult();
+    variableMap = new HashMap<String, Object>();
+    variableMap.put("pass", false);
+    taskService.complete(task.getId(), variableMap);
+
+    Task task2 = taskService.createTaskQuery().singleResult();
+    assertNotNull(task2);
+    taskService.complete(task2.getId());
+  }
+}
