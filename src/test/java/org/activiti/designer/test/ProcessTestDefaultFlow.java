ca161005acf2d32be597dbf5d857a25e1a3301e2
Henry Yan <yanhonglei@gmail.com>
2013-03-10 13:41:47 +0800

    添加defaultflow测试

---
+++ src/test/java/org/activiti/designer/test/ProcessTestDefaultFlow.java
@@ -0,0 +1,40 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestDefaultFlow {
+
+  private String filename = "/Users/henryyan/work/projects/activiti/activiti-study/src/test/resources/diagrams/qun/DefaultFlow.bpmn";
+
+  @Rule
+  public ActivitiRule activitiRule = new ActivitiRule();
+
+  @Test
+  public void startProcess() throws Exception {
+    RepositoryService repositoryService = activitiRule.getRepositoryService();
+    repositoryService.createDeployment().addInputStream("DefaultFlow.bpmn20.xml", new FileInputStream(filename)).deploy();
+    RuntimeService runtimeService = activitiRule.getRuntimeService();
+    Map<String, Object> variableMap = new HashMap<String, Object>();
+    variableMap.put("type", "2");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("DefaultFlow", variableMap);
+    assertNotNull(processInstance.getId());
+    System.out.println("id " + processInstance.getId() + " " + processInstance.getProcessDefinitionId());
+    
+    TaskService taskService = activitiRule.getTaskService();
+    Task task = taskService.createTaskQuery().singleResult();
+    assertEquals("usertask2", task.getTaskDefinitionKey());
+  }
+}
