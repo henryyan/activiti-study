c183c46ccb4df5bfad11e2d0f2563e7a333a4c29
Henry Yan <yanhonglei@gmail.com>
2013-04-13 16:41:26 +0800

    并行分支中其中一个分支回转到流程开始（再次完成任务会再创建2个任务)

---
+++ src/test/java/org/activiti/designer/test/ProcessTestQuitFlowOnParallel.java
@@ -0,0 +1,62 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestQuitFlowOnParallel {
+
+  private String filename = "/Users/henryyan/work/projects/activiti/activiti-study/src/test/resources/diagrams/qun/quitFlowOnParallel.bpmn";
+
+  @Rule
+  public ActivitiRule activitiRule = new ActivitiRule();
+
+  @Test
+  public void startProcess() throws Exception {
+    RepositoryService repositoryService = activitiRule.getRepositoryService();
+    repositoryService.createDeployment().addInputStream("quitFlowOnParallel.bpmn20.xml", new FileInputStream(filename)).deploy();
+    RuntimeService runtimeService = activitiRule.getRuntimeService();
+    Map<String, Object> variableMap = new HashMap<String, Object>();
+    variableMap.put("name", "Activiti");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("quitFlowOnParallel", variableMap);
+    assertNotNull(processInstance.getId());
+    System.out.println("id " + processInstance.getId() + " " + processInstance.getProcessDefinitionId());
+    
+    // task 1111111
+    TaskService taskService = activitiRule.getTaskService();
+    Task task111111 = taskService.createTaskQuery().taskName("11111").singleResult();
+    taskService.complete(task111111.getId());
+    
+    // 两个并行任务
+    long count = taskService.createTaskQuery().count();
+    assertEquals(2, count);
+    
+    // 完成任务：22222222，回到任务11111
+    Task task22222222 = taskService.createTaskQuery().taskName("22222222").singleResult();
+    variableMap = new HashMap<String, Object>();
+    variableMap.put("type", "back");
+    taskService.complete(task22222222.getId(), variableMap);
+    
+    // 统计任务数量
+    count = taskService.createTaskQuery().count();
+    assertEquals(2, count);
+    
+    // 再次完成任务111111
+    task111111 = taskService.createTaskQuery().taskName("11111").singleResult();
+    taskService.complete(task111111.getId());
+    
+    count = taskService.createTaskQuery().count();
+    assertEquals(3, count);
+  }
+}
