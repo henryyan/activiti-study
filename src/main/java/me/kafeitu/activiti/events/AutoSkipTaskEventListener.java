1656c4fc206b438749094d3d9839d4281486ab80
Henry Yan <yanhonglei@gmail.com>
2014-07-24 17:55:10 +0800

    节点自动跳过支持候选类型

--- src/main/java/me/kafeitu/activiti/events/AutoSkipTaskEventListener.java
+++ src/main/java/me/kafeitu/activiti/events/AutoSkipTaskEventListener.java
@@ -1,25 +1,37 @@
 package me.kafeitu.activiti.events;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.activiti.engine.EngineServices;
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.ManagementService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.delegate.event.ActivitiEntityEvent;
 import org.activiti.engine.delegate.event.ActivitiEvent;
 import org.activiti.engine.delegate.event.ActivitiEventListener;
 import org.activiti.engine.delegate.event.ActivitiEventType;
+import org.activiti.engine.identity.Group;
 import org.activiti.engine.impl.cmd.CompleteTaskCmd;
 import org.activiti.engine.impl.persistence.entity.TaskEntity;
+import org.activiti.engine.task.IdentityLink;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * @author: Henry Yan
  */
 public class AutoSkipTaskEventListener implements ActivitiEventListener {
+
+    public static Set<String> skipedTasks = new HashSet<>();
+
     @Override
     public void onEvent(ActivitiEvent event) {
-        if (event.getType().equals(ActivitiEventType.TASK_ASSIGNED)) {
+        if (event.getType().equals(ActivitiEventType.TASK_CREATED)
+                || event.getType().equals(ActivitiEventType.TASK_ASSIGNED)) {
             ActivitiEntityEvent entityEvent = (ActivitiEntityEvent) event;
 
             Object entity = entityEvent.getEntity();
@@ -27,30 +39,77 @@ public class AutoSkipTaskEventListener implements ActivitiEventListener {
                 TaskEntity taskEntity = (TaskEntity) entityEvent.getEntity();
 
                 Map<String, String> skipTasks = new HashMap<>();
-                skipTasks.put("usertask2", "user1");
-                skipTasks.put("usertask4", "user1");
+                skipTasks.put("usertask2", "user1"); // 预先设定
+                skipTasks.put("usertask4", "user1"); // 动态设定以
+                skipTasks.put("usertask5", "user1"); // 候选人
+                skipTasks.put("usertask6", "user1"); // 候选组
 
-                // 如果当前节点是需要跳过的，直接调用任务完成命令
+                EngineServices engineServices = entityEvent.getEngineServices();
+                IdentityService identityService = engineServices.getIdentityService();
+                TaskService taskService = engineServices.getTaskService();
+
                 String taskDefinitionKey = taskEntity.getTaskDefinitionKey();
                 String userId = skipTasks.get(taskDefinitionKey);
-                if (userId != null && taskEntity.getAssignee().equals(userId)) {
-                    EngineServices engineServices = entityEvent.getEngineServices();
-                    ManagementService managementService = engineServices.getManagementService();
 
-                    // 设置local级别的变量
-                    Map<String, Object> variables = new HashMap<String, Object>();
-                    variables.put("skip", true);
-                    CompleteTaskCmd command = new CompleteTaskCmd(taskEntity.getId(), variables, true);
+                if (userId == null) {
+                    return;
+                }
+                System.out.println("key: " + taskDefinitionKey + ", userId=" + userId);
 
-                    // 执行命令，直接完成当前任务
-                    System.out.println("准备跳过任务：" + taskDefinitionKey);
-                    managementService.executeCommand(command);
-                    System.out.println("已跳过任务：" + taskDefinitionKey);
+                // 查询当前人的组
+                List<String> groupIds = new ArrayList<>();
+                List<Group> list = identityService.createGroupQuery().groupMember(userId).list();
+                for (Group group : list) {
+                    groupIds.add(group.getId());
+                }
+
+                // 如果当前节点是需要跳过的，直接调用任务完成命令
+                if (event.getType().equals(ActivitiEventType.TASK_ASSIGNED)) {
+                    if (userId != null) {
+                        ManagementService managementService = engineServices.getManagementService();
+                        if (taskEntity.getAssignee().equals(userId)) {
+                            skipTask(taskEntity, managementService);
+                        }
+                    }
+                }
+
+                // 创建事件用来处理候选的情况
+                if (event.getType().equals(ActivitiEventType.TASK_CREATED) && taskEntity.getAssignee() == null) {
+                    Set<IdentityLink> candidates = taskEntity.getCandidates();
+                    for (IdentityLink identityLink : candidates) {
+                        // 处理候选人
+                        if (identityLink.getUserId() != null && identityLink.getUserId().equals(userId)) {
+                            taskEntity.deleteUserIdentityLink(identityLink.getUserId(), "candidate");
+                            taskService.claim(taskEntity.getId(), userId);
+                        } else if (groupIds.contains(identityLink.getGroupId())) {
+                            // 处理候选组
+                            taskService.claim(taskEntity.getId(), userId);
+                            taskEntity.deleteGroupIdentityLink(identityLink.getGroupId(), "candidate");
+                        }
+                    }
                 }
             }
         } else {
             System.out.println("拦截事件：" + ToStringBuilder.reflectionToString(event));
         }
+    }
+
+    private void skipTask(TaskEntity taskEntity, ManagementService managementService) {
+        String taskDefinitionKey = taskEntity.getTaskDefinitionKey();
+        if (skipedTasks.contains(taskDefinitionKey)) {
+            return;
+        }
+        // 设置变量
+        Map<String, Object> variables = new HashMap<String, Object>();
+        variables.put("skip", true);
+        CompleteTaskCmd command = new CompleteTaskCmd(taskEntity.getId(), variables, true);
+
+        // 执行命令，直接完成当前任务
+//        System.out.println("准备跳过任务：" + taskEntity.getName() + "-->" + taskDefinitionKey);
+        managementService.executeCommand(command);
+        System.out.println("已跳过任务：" + taskEntity.getName() + "-->" + taskDefinitionKey);
+
+        skipedTasks.add(taskDefinitionKey);
     }
 
     @Override
--- src/test/java/org/activiti/designer/test/AutoSkipTaskTest.java
+++ src/test/java/org/activiti/designer/test/AutoSkipTaskTest.java
@@ -1,7 +1,10 @@
 package org.activiti.designer.test;
 
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.test.ActivitiRule;
@@ -26,9 +29,24 @@ public class AutoSkipTaskTest {
     @Test
     @Deployment(resources = "diagrams/threeTask.bpmn")
     public void testSkip() {
+
+        // 建立用户与组的关系
+        IdentityService identityService = activitiRule.getIdentityService();
+        User user1 = identityService.newUser("user1");
+        identityService.saveUser(user1);
+        User user2 = identityService.newUser("user2");
+        identityService.saveUser(user2);
+
+        Group group1 = identityService.newGroup("group1");
+        identityService.saveGroup(group1);
+        identityService.createMembership("user1", "group1");
+
+        // 启动流程
         RuntimeService runtimeService = activitiRule.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("threeTask");
         System.out.println("已启动流程：" + processInstance.getId());
+
+        // 完成第一个任务
         TaskService taskService = activitiRule.getTaskService();
         Task task = taskService.createTaskQuery().singleResult();
         assertEquals("user1", task.getAssignee());
@@ -49,8 +67,9 @@ public class AutoSkipTaskTest {
         // 为usertask4设置办理人
         taskService.setAssignee(task.getId(), "user1");
 
-        // 任务到达usertask5
+        // 任务到达usertask7
         task = taskService.createTaskQuery().singleResult();
-        assertEquals("usertask5", task.getTaskDefinitionKey());
+        // taskService.claim(task.getId(), "user1");
+        assertEquals("目的地", task.getName());
     }
 }
